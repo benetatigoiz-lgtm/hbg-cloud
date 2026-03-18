1aa6722d127968a2c4104fe561e2b9e552f44a1d
benetatigoiz-lgtm <benetati.goiz@gmail.com>
2026-03-18 20:33:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,21 @@ app.get("/api/v1/health", (req, res) => {
 res.json({ status: "ok" });
 });
 
+app.post("/api/v1/dados", (req, res) => {
+const dados = req.body;
+
+console.log("Dados recebidos:");
+console.log(dados);
+
+res.json({ status: "recebido" });
+});
+
 app.get("/", (req, res) => {
 res.send("HBG Cloud funcionando");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen( PORT , () => {
-  console.log("Servidor rodando na porta" + PORT);
+
+app.listen(PORT, () => {
+console.log("Servidor rodando na porta " + PORT);
 });
